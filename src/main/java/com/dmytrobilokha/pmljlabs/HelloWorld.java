f4b5e177dd7cabd2b68f4d88f41f2e54bc80fd10
Dmytro Bilokha <dmytro@posteo.net>
2024-12-03 21:25:27 +0100

    Added Apache Commons Math dependency and runner script

--- src/main/java/com/dmytrobilokha/pmljlabs/HelloWorld.java
+++ src/main/java/com/dmytrobilokha/pmljlabs/HelloWorld.java
@@ -1,8 +1,21 @@
 package com.dmytrobilokha.pmljlabs;
 
+import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.LUDecomposition;
+import org.apache.commons.math3.linear.MatrixUtils;
+import org.apache.commons.math3.linear.RealMatrix;
+
 public class HelloWorld {
 
     public static void main(String[] cliArgs) {
+        double[][] matrixData = { {1d,2d,3d}, {2d,5d,3d}};
+        RealMatrix m = MatrixUtils.createRealMatrix(matrixData);
+        double[][] matrixData2 = { {1d,2d}, {2d,5d}, {1d, 7d}};
+        RealMatrix n = new Array2DRowRealMatrix(matrixData2);
+        RealMatrix p = m.multiply(n);
+        System.out.println(p.getRowDimension());    // 2
+        System.out.println(p.getColumnDimension()); // 2
+        RealMatrix pInverse = new LUDecomposition(p).getSolver().getInverse();
         System.out.println("All is OK");
     }
 
